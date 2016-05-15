b7a59dd0443d5a0d58d8ff92b90b84cbd78ac8ff
Oren Farhi <oren.farhi-contractor@adp.com>
2016-05-15 10:57:54 +0300

    rmeoving app folder

--- app/main.js
+++
@@ -1,5 +0,0 @@
-(function(app) {
-  document.addEventListener('DOMContentLoaded', function() {
-    ng.platformBrowserDynamic.bootstrap(app.AppComponent);
-  });
-})(window.app || (window.app = {}));
