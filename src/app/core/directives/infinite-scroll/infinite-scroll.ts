3a22d5809faae382afe2c4a1c581fe8c3ce0a3a0
orizens <farhioren@gmail.com>
2016-05-30 11:20:13 +0300

    updated inifinite scroll

--- src/app/core/directives/infinite-scroll/infinite-scroll.ts
+++ src/app/core/directives/infinite-scroll/infinite-scroll.ts
@@ -1,31 +1,31 @@
-// import {isPresent} from 'angular2/src/facade/lang';
-import {Directive, ElementRef, Input, Output, EventEmitter} from '@angular/core';
+import { Directive, ElementRef, Input, Output, EventEmitter, OnDestroy, OnInit } from '@angular/core';
 import { window } from '@angular/platform-browser/src/facade/browser';
 import { Scroller } from './scroller';
 
-/**
- * Infinite Scroll for Angular 2
- *
- * ## Use
- *
- * ```
- * <li router-active="active"><a [routerLink]=" ['/Home'] ">Home</a></li>
- * <li [routerActive]=" activeStringValue "><a [routerLink]=" ['/Home'] ">Home</a></li>
- * ```
- */
 @Directive({
   selector: '[infinite-scroll]'
 })
-export class InfiniteScroll {
-  @Input() infiniteScrollDistance: Number;
-  @Output() scrolled = new EventEmitter();
+export class InfiniteScroll implements OnDestroy, OnInit {
   private scroller: Scroller;
+  private _distance: number = 2;
 
-  constructor( private element: ElementRef ) {
-    this.scroller = new Scroller(window, setInterval, element, this.onScroll.bind(this), 2, {});
-  };
+  @Input() set infiniteScrollDistance(distance: number) {
+    this._distance = distance;
+  }
 
-  onScroll () {
+  @Output() scrolled = new EventEmitter();
+
+  constructor(private element: ElementRef) {}
+
+  ngOnInit() {
+    this.scroller = new Scroller(window, setInterval, this.element, this.onScroll.bind(this), this._distance, {});
+  }
+
+  ngOnDestroy () {
+    this.scroller.clean();
+  }
+  
+  onScroll() {
     this.scrolled.next({});
   }
 }
