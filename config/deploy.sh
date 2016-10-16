26ff7f18785860f19bf53847d57ad3e6ea30063c
Oren Farhi <farhioren@gmail.com>
2016-10-16 12:37:15 +0300

    updated deploy script

--- config/deploy.sh
+++ config/deploy.sh
@@ -3,9 +3,13 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
   git config --global user.email "farhioren+travis@gmail.com"
   git config --global user.name "travis-ci"
   npm run build:prod
-  cd dist
-  git init
+  git checkout gh-pages
+  rm *.*
+  rm -rf fonts
+  rm -rf assets
+  rm -rf coverage
+  cp -av dist/ ./
   git add .
   git commit -m "deployed commit ${TRAVIS_COMMIT} from travis"
-  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+  git push "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 fi
