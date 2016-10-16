e09991ba541e91a787449679d70fe48f500bc885
Oren Farhi <farhioren@gmail.com>
2016-10-16 12:51:27 +0300

    added logs to travis yml

--- config/deploy.sh
+++ config/deploy.sh
@@ -3,13 +3,11 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
   git config --global user.email "farhioren+travis@gmail.com"
   git config --global user.name "travis-ci"
   npm run build:prod
-  git checkout gh-pages
-  rm *.*
-  rm -rf fonts
-  rm -rf assets
-  rm -rf coverage
-  cp -av dist/ ./
+  git status
+  git remote -v
+  cd dist
+  git init
   git add .
   git commit -m "deployed commit ${TRAVIS_COMMIT} from travis"
-  git push "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 fi
