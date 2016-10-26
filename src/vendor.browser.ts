47057a43d434954e9f0a29fbf96bb6c260379ff0
Oren Farhi <farhioren@gmail.com>
2016-10-26 09:40:28 +0300

    added all rx js

--- src/vendor.browser.ts
+++ src/vendor.browser.ts
@@ -18,15 +18,15 @@ import '@angular/router';
 import '@angularclass/hmr';
 
 // RxJS
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/switchMapTo';
-import 'rxjs/add/operator/take';
-import 'rxjs/add/operator/combineLatest';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/mergeMap';
+// import 'rxjs/add/operator/switchMapTo';
+// import 'rxjs/add/operator/take';
+// import 'rxjs/add/operator/combineLatest';
 
-import 'rxjs/add/observable/of';
-import 'rxjs/add/observable/fromPromise';
-import 'rxjs/add/observable/toPromise';
+// import 'rxjs/add/observable/of';
+// import 'rxjs/add/observable/fromPromise';
+// import 'rxjs/add/observable/toPromise';
 
 import 'rxjs/Rx';
 
