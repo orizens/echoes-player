be817a483288d19d4cee2b16db96bc3b0260925a
Oren Farhi <farhioren@gmail.com>
2016-11-25 10:30:14 +0200

    added: action creator factory for ngrx/store actions

---
+++ src/app/core/store/action-creator.util.ts
@@ -0,0 +1,21 @@
+import { Action } from '@ngrx/store';
+
+/* Action Creator Factory
+ * ======================
+ * Creates typed action creators functions
+ * 
+ * const addTodo = ActionCreatorFactory.create<string>('ADD_TODO')
+ * 
+ **/
+class ActionCreator<T> implements Action {
+  constructor(
+    public type: string = 'NOT_SET',
+    public payload?: T
+  ) {}
+}
+
+export class ActionCreatorFactory {
+  static create?<T>(type: string) {
+    return (payload?: T) => new ActionCreator<T>(type, payload);
+  }
+}
