169f459a42d50d7069fcbbc7cd6b430d540bc034
Oren Farhi <farhioren@gmail.com>
2017-03-10 00:24:55 +0200

    seperated playlist resolvers

--- src/app/user/playlist/index.ts
+++ src/app/user/playlist/index.ts
@@ -1,3 +1,4 @@
 export { PlaylistComponent } from './playlist.component';
 export { PlaylistCoverComponent } from './playlist-cover.component';
 export * from './playlist.resolver';
+export * from './playlist-videos.resolver';
--- src/app/user/playlist/playlist-videos.resolver.ts
+++ src/app/user/playlist/playlist-videos.resolver.ts
@@ -4,14 +4,13 @@ import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class PlaylistResolver implements Resolve<any> {
+export class PlaylistVideosResolver implements Resolve<any> {
   constructor(
     private userProfile: UserProfile,
   ) {}
 
-  resolve(route: ActivatedRouteSnapshot): Observable<GoogleApiYouTubePlaylistResource> {
+  resolve(route: ActivatedRouteSnapshot): Observable<GoogleApiYouTubeVideoResource[]> {
     const playlistId = route.params['id'];
-    return this.userProfile.fetchPlaylist(playlistId)
-      .map(response => response.items[0]);
+    return this.userProfile.fetchPlaylistItems(playlistId);
   }
 }
