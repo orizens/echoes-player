ff40bcedc9b76d0fb2aab04664bb99b7b383eccf
Oren Farhi <farhioren@gmail.com>
2017-04-23 10:42:27 +0300

    removed unused

--- src/app/containers/app-navbar/app-navbar.component.ts
+++ src/app/containers/app-navbar/app-navbar.component.ts
@@ -66,7 +66,6 @@ export class AppNavbarComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    console.log('navbar init now...');
   }
 
   signInUser () {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,6 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-const EchoesPackage = require('../../package.json');
 export const environment = {
-  production: false,
-  package: EchoesPackage
+  production: false
 };
