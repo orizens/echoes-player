796b303e17e391b0f59dbdb7d818aa15c07025d5
Oren Farhi <farhioren@gmail.com>
2018-06-11 19:53:14 +0300

    add(pwa):  adding sw support, icons

--- src/environments/environment.integration.ts
+++ src/environments/environment.integration.ts
@@ -3,7 +3,7 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
-  production: false,
+  production: true,
   youtube: {
     API_KEY: 'AIzaSyDCGg6FG6s_zxACqel09vQUKBc-x26pKFA',
     CLIENT_ID: '971861197531-hm7solf3slsdjc4omsfti4jbcbe625hs'
