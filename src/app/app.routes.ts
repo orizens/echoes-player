b5567a8682f0ae7d532d13482953070a32acdc35
Oren Farhi <farhioren@gmail.com>
2020-06-25 10:48:48 -0400

    update: latest ng-8

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes, RouterModule } from '@angular/router';
 
 export const ROUTES: Routes = [
   { path: '', redirectTo: 'search', pathMatch: 'full' },
-  { path: 'user', loadChildren: 'app/containers/user/index#UserModule' }
+  { path: 'user', loadChildren: () => import('./containers/user/index').then(m => m.UserModule) }
 ];
